61edfba08d4743c3e4d67da703d5e3818f157c69
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-08-23 02:40:53 +0000

    load trt engine

--- trt_py/main.py
+++ trt_py/main.py
@@ -9,4 +9,10 @@ class TensorRTInfer:
     def __init__(self, engine_path : str) -> None:
         
         self.logger = MyLogger()
-        self.runtime = trt.Runtime(self.logger)
+        self.runtime = trt.Runtime(self.logger) 
+
+        # Load TRT Engine
+        with open(engine_path, "rb") as f:
+            serialized_engine = f.read() 
+            self.engine = self.runtime.deserialize_cuda_engine(serialized_engine) 
+        self.context = self.engine.create_execution_context()
