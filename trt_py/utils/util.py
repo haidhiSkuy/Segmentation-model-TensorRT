9d665a1f301b9a2fd36f26dcf7eb257e224d5b47
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-08-23 02:50:47 +0000

    postprocess

--- trt_py/utils/util.py
+++ trt_py/utils/util.py
@@ -1,5 +1,9 @@
 import os
+import numpy as np
 
 def is_image(path):
     extensions = [".jpg", ".jpeg", ".png", ".bmp"]
     return (os.path.isfile(path) and os.path.splitext(path)[1].lower() in extensions)  
+
+def sigmoid(x): 
+    return 1 / (1 + np.exp(-x))
