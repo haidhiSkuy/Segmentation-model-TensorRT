d7a45e751c49510406295824653a00f12ca69160
Haidhi Angkawijana <haidhiangkawijana@gmail.com>
2024-08-27 13:47:52 +0000

    tes input image

--- trt_cpp/main.cpp
+++ trt_cpp/main.cpp
@@ -4,12 +4,12 @@
 #include "NvInfer.h" 
 
 
-
-
 int main()
 { 
     SegmentationInfer segmen("/workspaces/tensorRT/model/ghost_unet.engine");
-
+    
+    std::string image_path = "/workspaces/tensorRT/sample_image/image4.png"; 
+    segmen.preprocessing(image_path);  
     
     return 0; 
 }
